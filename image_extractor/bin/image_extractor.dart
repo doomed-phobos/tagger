a1ff294fc69000252a7c5361e47d901b7cb63779
doomed-phobos <the_ekj.2121@outlook.com>
2026-03-26 00:10:45 -0500

    Just show the url

--- image_extractor/bin/image_extractor.dart
+++ image_extractor/bin/image_extractor.dart
@@ -1,5 +1,8 @@
 import 'package:image_extractor/image_extractor.dart' as image_extractor;
 
-void main(List<String> arguments) {
-  print('Hello world: ${image_extractor.calculate()}!');
+void main(List<String> arguments) async {
+  final task = image_extractor.get_image_url_from_hitomi_url("https://hitomi.la/reader/3855946.html#2-3");
+  final o = task.run();
+
+  print(await o);
 }
