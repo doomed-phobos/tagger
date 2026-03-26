d08d1c66f3d3ddeb2bbd9c7e048f367bc17ac394
doomed-phobos <the_ekj.2121@outlook.com>
2026-03-25 20:41:39 -0500

    Initial Commit

---
+++ image_extractor/bin/image_extractor.dart
@@ -0,0 +1,5 @@
+import 'package:image_extractor/image_extractor.dart' as image_extractor;
+
+void main(List<String> arguments) {
+  print('Hello world: ${image_extractor.calculate()}!');
+}
---
+++ image_extractor/test/image_extractor_test.dart
@@ -0,0 +1,8 @@
+import 'package:image_extractor/image_extractor.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
