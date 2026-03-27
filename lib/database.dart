38905b18ea2e9303ea583798581f048c6a23152b
doomed-phobos <the_ekj.2121@outlook.com>
2026-03-26 22:31:15 -0500

    Added "add" function

--- lib/database.dart
+++ lib/database.dart
@@ -11,14 +11,38 @@ class Database {
   final String _directory_path;
   final List<Artist> _artists;
   final List<Tag> _tags;
+  final HashMap<NonEmptyString/* artist_name */, int /* index in list */> _map_artists;
   final HashMap<int /* tag_id */, int /* index in list */> _map_tags;
 
-  List<Tag> get tags => List.unmodifiable(_artists);
+  List<Tag> get tags => List.unmodifiable(_tags);
+  List<Artist> get artists => List.unmodifiable(_artists);
+  
+  Database._(this._directory_path, this._artists, this._tags, this._map_artists, this._map_tags);
 
-  Database._(this._directory_path, this._artists, this._tags, this._map_tags);
+  Option<Tag> get_tag_by_id(int id) => _map_tags.lookup(id).map((i) => _tags[i]); 
 
-  void add(NonEmptyString artist_name, Iterator<(NonEmptyString, Uint8List)> tag_iterator, Iterator<NonEmptyString> url_iterator) {
-    
+  void add(
+    NonEmptyString artist_name,
+    Iterable<(NonEmptyString, Uint8List)> tags,
+    Iterable<NonEmptyString> urls) {
+    var artist_tags = <ArtistTag>[];
+    for(var e in tags) {
+      final tag_id = e.$1.hashCode;
+      if (!_map_tags.containsKey(tag_id)) {
+        _tags.add(Tag(id: tag_id, name:e.$1));
+        _map_tags[tag_id] = _tags.length-1;
+      }
+      artist_tags.add(ArtistTag(tag_id: tag_id, image_url: NonEmptyString.unsafeMake("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdUY6-53NESEHhJDAyfXsJigOm9_okUAsgjw&s")));
+    }
+
+    final artist = Artist(name: artist_name, tags: artist_tags, urls: urls.toList());
+    _map_artists.lookup(artist_name)
+      .match(
+        () {
+          _artists.add(artist);
+          _map_artists[artist_name] = _artists.length-1;
+        },
+        (i) => _artists[i] = artist);
   }
 
   static TaskOption<Database> make_from_data() => TaskOption(() async {
@@ -48,8 +72,9 @@ class Database {
       .getOrElse(() => [])
       .run();
 
-    final map_tags = HashMap<int, int>.fromIterable(tags.mapWithIndex((tag, i) => (tag.id, i)));
-    
-    return some(Database._(directory.path, artists, tags, map_tags));
+    final map_tags    = HashMap<int, int>.fromIterable(tags.mapWithIndex((tag, i) => (tag.id, i)));
+    final map_artists = HashMap<NonEmptyString, int>.fromIterable(artists.mapWithIndex((artist, i) => (artist.name, i)));
+
+    return some(Database._(directory.path, artists, tags, map_artists, map_tags));
   });
 }
