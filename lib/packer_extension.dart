373cd37e0b51535cadc61e9879ae1a4abbca7457
doomed-phobos <the_ekj.2121@outlook.com>
2026-03-28 10:45:21 -0500

    Utils

--- lib/packer_extension.dart
+++ lib/packer_extension.dart
@@ -2,6 +2,7 @@ import 'package:fpdart/fpdart.dart';
 import 'package:messagepack/messagepack.dart';
 
 extension SerializerExtension on Unpacker {
+  Option<bool> toOptionBool() => Option.tryCatch(() => unpackBool()).flatMapNullable((e) => e);
   Option<String> toOptionString() => Option.tryCatch(() => unpackString()).flatMapNullable((e) => e);
   Option<int> toOptionInt() => Option.tryCatch(() => unpackInt()).flatMapNullable((e) => e);
 }
