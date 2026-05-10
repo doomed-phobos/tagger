5f2763b999d45ec9a581b10d1d0af27ebb0174b9
doomed-phobos <the_ekj.2121@outlook.com>
2026-05-10 00:25:14 -0500

    feat(database): changed to custom database to sqlite database
    BREAKING CHANGE:

--- lib/packer_extension.dart
+++
@@ -1,8 +0,0 @@
-import 'package:fpdart/fpdart.dart';
-import 'package:messagepack/messagepack.dart';
-
-extension SerializerExtension on Unpacker {
-  Option<bool> toOptionBool() => Option.tryCatch(() => unpackBool()).flatMapNullable((e) => e);
-  Option<String> toOptionString() => Option.tryCatch(() => unpackString()).flatMapNullable((e) => e);
-  Option<int> toOptionInt() => Option.tryCatch(() => unpackInt()).flatMapNullable((e) => e);
-}
