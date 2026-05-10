bb77724d714086eb366d991f08f6cf7169f4c2c0
doomed-phobos <the_ekj.2121@outlook.com>
2026-05-09 21:41:33 -0500

    refactor(main): se adaptó al cambio de database

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import "package:fpdart/fpdart.dart";
 import 'package:tagger/bootstrap.dart';
-import 'package:tagger/db/database.dart';
+import "package:tagger/db/database.dart";
 import 'package:tagger/pages/home.dart';
 import 'package:toastification/toastification.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final either = Either.tryCatch(
+    () => Database(),
+    (s, _) => "Failed to initialize Database: $s",
+  );
 
   runApp(
-    await Database.make_from_data()
-        .match(
-          () => const MaterialApp(home: Text("Failed to initialize Database!!")),
-          (database) => ToastificationWrapper(child: bootstrap(HomePage(database))),
-        )
-        .run(),
+    either.match(
+      (error) => MaterialApp(home: Text(error)),
+      (database) => ToastificationWrapper(child: bootstrap(HomePage(database))),
+    ),
   );
 }
