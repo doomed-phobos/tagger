1e8719ba1c7042f0d2b2250428d50760dd31d4f5
doomed-phobos <the_ekj.2121@outlook.com>
2026-05-15 08:16:13 -0500

    style: single to double quote

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import "package:flutter/material.dart";
 import "package:fpdart/fpdart.dart";
-import 'package:tagger/bootstrap.dart';
+import "package:tagger/bootstrap.dart";
 import "package:tagger/db/database.dart";
-import 'package:tagger/pages/home.dart';
-import 'package:toastification/toastification.dart';
+import "package:tagger/pages/home.dart";
+import "package:toastification/toastification.dart";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
