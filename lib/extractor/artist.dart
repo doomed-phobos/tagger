57405bc0fe6ccf315bf611e4574c381a1084a751
doomed-phobos <the_ekj.2121@outlook.com>
2026-05-16 10:34:25 -0500

    feat(extractor): extraction for artist and last gallery id from url

---
+++ lib/extractor/artist.dart
@@ -0,0 +1,35 @@
+import "dart:typed_data";
+
+import "package:fpdart/fpdart.dart";
+import "package:http/http.dart" as http;
+
+typedef ArtistData = (String /*name*/, int /*last gallery id*/);
+
+TaskEither<String, ArtistData> get_artist_data_from_url(String url) => TaskEither(() async {
+  final regex = RegExp(
+    r"^https://hitomi\.la/artist/(.*)-all\.html$",
+  ).firstMatch(url);
+  if (regex == null) {
+    return left("Invalid URL artist");
+  }
+  final name = regex.group(1)!;
+
+  final response = await http.get(
+    Uri.parse(
+      'https://ltn.gold-usergeneratedcontent.net/artist/$name-all.nozomi',
+    ),
+    headers: {'Range': 'bytes=0-4'},
+  );
+
+  if (!(response.statusCode == 200 || response.statusCode == 206)) {
+    return left("Failed to get artist info");
+  }
+
+  final bytes = response.bodyBytes;
+  final data = ByteData.sublistView(bytes);
+
+  return right(
+  (
+  Uri.decodeComponent(name),
+  data.getInt32(0, Endian.big)));
+});
