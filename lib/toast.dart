10d9146a935fac4ca0ea8d52bb261f3477c5b9d4
doomed-phobos <the_ekj.2121@outlook.com>
2026-05-16 10:36:39 -0500

    refactor: moved toast to individual file for resusing

---
+++ lib/toast.dart
@@ -0,0 +1,22 @@
+import "package:toastification/toastification.dart";
+import 'package:flutter/material.dart';
+
+const TOAST_DURATION = Duration(seconds: 3);
+
+void show_error_toast(String text) {
+  toastification.show(
+    title: Text(text),
+    type: .error,
+    alignment: Alignment.bottomRight,
+    autoCloseDuration: TOAST_DURATION,
+  );
+}
+
+void show_success_toast(String text) {
+  toastification.show(
+    title: Text(text),
+    type: .success,
+    alignment: Alignment.bottomRight,
+    autoCloseDuration: TOAST_DURATION,
+  );
+}
